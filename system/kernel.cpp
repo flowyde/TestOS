b8695ae48c3d8078caac9a8ca8c4d5471e5b9e06
flowyde <69410739+flowyde@users.noreply.github.com>
2025-08-02 13:51:39 -0300

    corrected some contructor errors

--- system/kernel.cpp
+++ system/kernel.cpp
@@ -6,8 +6,16 @@ void printf(char* str){
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
+typedef void (*constructor)();
+extern "C" constructor   start_ctors;
+extern "C" constructor   end_ctors;
+extern "C" void callConstructors()
+{
+    for (constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber){
+extern "C" void kernelMain(const void* multiboot_structure, unsigned int magicnumber){
     printf("abacaxi eh muito bomm");
 
     while(1);
