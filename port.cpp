f9a36cdbae2acd8b5a63e78768fca0d4ef40ece9
flowyde <69410739+flowyde@users.noreply.github.com>
2025-08-02 22:00:45 -0300

    Update port.cpp

--- port.cpp
+++ port.cpp
@@ -22,5 +22,51 @@ void Port8Bit::Write(uint8_t data){
 }
 
 uint8_t Port8Bit::Read(){
+    uint8_t result;
+    __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (portnumber));
+    return result;
+}
 
+
+
+
+
+Port16Bit::Port16Bit(uint16_t portnumber)
+: Port(portnumber){
+
+}
+Port16Bit::~Port16Bit(){
+
+}
+
+void Port16Bit::Write(uint16_t data){
+    __asm__ volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
+}
+
+uint16_t Port16Bit::Read(){
+    uint16_t result;
+    __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (portnumber));
+    return result;
+}
+
+
+
+
+
+Port32Bit::Port32Bit(uint32_t portnumber)
+: Port(portnumber){
+
+}
+Port32Bit::~Port32Bit(){
+
+}
+
+void Port32Bit::Write(uint32_t data){
+    __asm__ volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
+}
+
+uint32_t Port32Bit::Read(){
+    uint32_t result;
+    __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (portnumber));
+    return result;
 }
